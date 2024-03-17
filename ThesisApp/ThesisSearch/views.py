8b55f6951e32a359f71e2a66f49ee4d121725530
kiel019 <202080497@psu.palawan.edu.ph>
2024-03-17 22:01:38 +0800

    add keyword function

--- ThesisApp/ThesisSearch/views.py
+++ ThesisApp/ThesisSearch/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Thesis
 
 def search_thesis(request):
     query = request.GET.get('q')
     if query:
-        theses = Thesis.objects.filter(title__icontains=query)
+        # Search by title or keywords
+        theses = Thesis.objects.filter(Q(title__icontains=query) | Q(keywords__word__icontains=query))
     else:
         theses = Thesis.objects.all()
     return render(request, 'search.html', {'theses': theses})
