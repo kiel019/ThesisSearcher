46bb6dc0871a77b4ab180f377b2cd9a31cde4f12
kiel019 <202080497@psu.palawan.edu.ph>
2024-03-17 21:42:33 +0800

    Fixed bugs to thesis search

--- ThesisApp/ThesisSearch/views.py
+++ ThesisApp/ThesisSearch/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Thesis
 
-def thesis_list(request):
-    theses = Thesis.objects.all()
-    return render(request, 'ThesisSearch/thesis_list.html', {'theses': theses})
+def search_thesis(request):
+    query = request.GET.get('q')
+    if query:
+        theses = Thesis.objects.filter(title__icontains=query)
+    else:
+        theses = Thesis.objects.all()
+    return render(request, 'search.html', {'theses': theses})
